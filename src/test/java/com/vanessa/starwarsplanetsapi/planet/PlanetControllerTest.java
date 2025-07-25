0aeee88dd7de5aeb29e6bd74780ca6e02c4d061f
Vanessa <vderigoesteves@gmail.com>
2025-07-25 13:35:50 -0300

    test(PlanetController): ensure 409 Conflict is returned when creating a planet with existing name

--- src/test/java/com/vanessa/starwarsplanetsapi/planet/PlanetControllerTest.java
+++ src/test/java/com/vanessa/starwarsplanetsapi/planet/PlanetControllerTest.java
@@ -7,13 +7,18 @@ import com.vanessa.starwarsplanetsapi.planet.service.PlanetService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.Optional;
+
 import static com.vanessa.starwarsplanetsapi.commom.PlanetConstants.INVALID_PLANET;
 import static com.vanessa.starwarsplanetsapi.commom.PlanetConstants.PLANET;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -52,5 +57,15 @@ public class PlanetControllerTest {
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(INVALID_PLANET)))
                 .andExpect(status().isUnprocessableEntity());
+    }
+
+    @Test
+    public void createPlanet_WithExistingName_ReturnsConflict() throws Exception{
+        when(service.create(any())).thenThrow(DataIntegrityViolationException.class);
+
+        mockMvc.perform(post("/planets")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(PLANET)))
+                .andExpect(status().isConflict());
     }
 }
