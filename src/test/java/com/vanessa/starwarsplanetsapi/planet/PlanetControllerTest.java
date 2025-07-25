7f6b47b3bb9b9e4939b505225cbda8b1de40c029
Vanessa <vderigoesteves@gmail.com>
2025-07-25 01:49:58 -0300

    test: add integration test for PlanetController create with invalid data returning 422

--- src/test/java/com/vanessa/starwarsplanetsapi/planet/PlanetControllerTest.java
+++ src/test/java/com/vanessa/starwarsplanetsapi/planet/PlanetControllerTest.java
@@ -2,6 +2,7 @@ package com.vanessa.starwarsplanetsapi.planet;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.vanessa.starwarsplanetsapi.planet.controller.PlanetController;
+import com.vanessa.starwarsplanetsapi.planet.domain.Planet;
 import com.vanessa.starwarsplanetsapi.planet.service.PlanetService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,7 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static com.vanessa.starwarsplanetsapi.commom.PlanetConstants.INVALID_PLANET;
 import static com.vanessa.starwarsplanetsapi.commom.PlanetConstants.PLANET;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -35,5 +37,20 @@ public class PlanetControllerTest {
                 .andExpect(status().isCreated())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
                 .andExpect(jsonPath("$").value(PLANET));
+    }
+
+    @Test
+    public void createPlanet_WithInvalidData_ReturnsBadRequest() throws Exception {
+        Planet emptyPlanet = new Planet();
+
+        mockMvc.perform(post("/planets")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(emptyPlanet)))
+                .andExpect(status().isUnprocessableEntity());
+
+        mockMvc.perform(post("/planets")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(INVALID_PLANET)))
+                .andExpect(status().isUnprocessableEntity());
     }
 }
